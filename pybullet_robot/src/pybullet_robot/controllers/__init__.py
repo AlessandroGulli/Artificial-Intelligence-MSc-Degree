f35b0c7c0f16d5d7c15e8bbe2935d3f4e14c5621
AlessandroGulli <alessandrogulli87@alice.it>
2022-03-21 16:07:14 +0100

    Delete pybullet_robot directory

--- pybullet_robot/src/pybullet_robot/controllers/__init__.py
+++
@@ -1,3 +0,0 @@
-from pybullet_robot.controllers.os_impedance_ctrl import OSImpedanceController
-from pybullet_robot.controllers.os_impedance_ctrl_joint_space import OSImpedanceControllerJointSpace
-from pybullet_robot.controllers.os_hyb_ctrl import OSHybridController
--- pybullet_robot/src/pybullet_robot/robots/bullet_panda/panda_robot_config.py
+++
@@ -1,5 +0,0 @@
-
-ROBOT_CONFIG = {
-    'ee_link_idx': 8,
-    'ee_link_name': 'panda_hand'
-}
